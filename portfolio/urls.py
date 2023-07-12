2a614679ff81a1a57ba477093dd82b333a03b90a
Fabianbatistasilva <currifabianbatista@gmail.com>
2023-07-12 11:18:26 -0300

    add urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path
-from . import views
+from portfolio.views import *
 
 urlpatterns = [
-   path('', views.home , name='home'),
-   path('sobre_Fabian/', views.sobre , name='sobre'),
-   path('Oferta/', views.oferta , name='oferta')
+   path('', home , name='home'),
+   path('sobre_Fabian/', sobre , name='sobre'),
+   path('oferta/', oferta , name='oferta'),
 ]
