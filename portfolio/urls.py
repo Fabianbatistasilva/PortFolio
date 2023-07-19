dde05e2c349988d4cdeb275913e61f5ae2b6ed75
Fabianbatistasilva <currifabianbatista@gmail.com>
2023-07-19 17:57:15 -0300

    update htmls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,5 @@ from portfolio.views import *
 urlpatterns = [
    path('', home , name='home'),
    path('sobre_Fabian/', sobre , name='sobre'),
-   path('oferta/', oferta , name='oferta'),
+   path('contato/', oferta , name='contato'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -23,37 +23,19 @@ def sobre(request):
 
 def oferta(request):
     if request.method=='POST':
-        Empresa = request.POST.get('nomeEmpresa')
-        Cargo = request.POST.get('cargo')
-        Local = request.POST.get('localizar')
-        Descricao = request.POST.get('descricao_vaga')
-        Salario = request.POST.get('salario_vaga')
-        if Empresa =='' and Cargo =='' and Local =='' and Descricao =='' and Salario =='':
+        Email = request.POST.get('email')
+        name = request.POST.get('name')
+        Descricao = request.POST.get('message')
+        if Email =='' and Cargo =='' and name =='' and Descricao =='' :
             messages.add_message(request,messages.INFO,f'Erro,Campo estão vazios')
             return render(request,'paginas/form_oferta.html',{})
-        if Cargo.isalpha()==False:
-            messages.add_message(request,messages.INFO,f'Erro, Cargo só aceita letras')
-            return render(request,'paginas/form_oferta.html',{})
-        if Cargo.isspace()==True :
-            messages.add_message(request,messages.INFO,f'Erro, Cargo com valor invalido')
-            return render(request,'paginas/form_oferta.html',{})
-        if Empresa.isspace() :
-            messages.add_message(request,messages.INFO,f'Erro, Empresa com valor invalido')
-            return render(request,'paginas/form_oferta.html',{})
-        if Salario.isdecimal()==False:
-            messages.add_message(request,messages.INFO,'Erro, Descrição com valor invalido')
-            return render(request,'paginas/form_oferta.html',{})
-        if Salario.isspace()==True :
-            messages.add_message(request,messages.INFO,f'Erro, Salario com valor invalido')
-            return render(request,'paginas/form_oferta.html',{})
         else:
-            Contrato =  request.POST.get('contato_vaga')
-            Tipo =  request.POST.get('tipo_vaga')
-            Email = request.POST.get('EmailEmpresa')
-            message= f' A {Empresa} ofereceu {Cargo} em {Local} \n Descrição da Vaga \n {Descricao} \n Salario: {Salario}\n Contrato:{Contrato} Tipo : {Tipo} Email:{Email}'
+            message= f' A/O {name} entrou em contato com {Email} Sobre \n {Descricao}'
             title= 'Oferta de emprego pelo Portfolio'
             email_enviado='currifabianbatista@gmail.com'
             para_email=['currifabianbatista@gmail.com']
             send_mail(title,message,email_enviado,para_email,fail_silently=False,)
             messages.add_message(request,messages.SUCCESS,'Enviou com Sucesso,Oferta foi enviada para o Email do Fabian')
-    return render(request,'paginas/form_oferta.html',{})
+            return render(request,'paginas/form_oferta.html',{})
+    if request.method == 'GET':
+        return render(request,'paginas/form_oferta.html',{})
