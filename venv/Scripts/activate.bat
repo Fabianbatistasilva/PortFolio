8242f27a6a80914f0b5479c419090751b3a08b1f
Fabianbatistasilva <currifabianbatista@gmail.com>
2024-02-25 12:33:21 -0300

    add projects

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\curri\OneDrive\Documents\VSCode\Portfolio\venv
+set VIRTUAL_ENV=C:\Users\Fabian\VSCODE\Portfolio\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
